f3085bc58af5926d579b9adebb8c41403de0d3d2
harinithak <127740017+harinithak@users.noreply.github.com>
2025-04-03 14:32:59 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u harinitha -p 21-Mar-05
-    docker tag test harinitha/task_h
-    docker push harinitha/task_h
+    docker tag test harinitha/devops_project1
+    docker push harinitha/devops_project1
     
